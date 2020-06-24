67865e3696380f997344485dac7eb3926e63818c
Patrick Wilson <pmwilson333@gmail.com>
2020-06-24 16:43:34 -0700

    finished button count

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const buttonCount = {
+  pink: 0,
+  blue: 0,
+  green: 0,
+};
+
 document.getElementById("name-input").addEventListener("keyup", updateName);
 document.getElementById("pink").addEventListener("click", changePink);
 document.getElementById("blue").addEventListener("click", changeBlue);
@@ -11,24 +17,33 @@ function updateName() {
 }
 
 function changePink() {
-  const elementToChange = document.getElementById("color-to-change");
+  const elementToChange = document.getElementById("color-to-change"),
+    buttonToUpdate = document.getElementById("pink");
 
+  updateCount("pink");
+  buttonToUpdate.textContent = "Pink (" + buttonCount.pink + ")";
   unSelectAllButtons();
   selectButton("pink");
   elementToChange.style.background = "pink";
 }
 
 function changeBlue() {
-  const elementToChange = document.getElementById("color-to-change");
+  const elementToChange = document.getElementById("color-to-change"),
+    buttonToUpdate = document.getElementById("blue");
 
+  updateCount("blue");
+  buttonToUpdate.textContent = "Blue (" + buttonCount.blue + ")";
   unSelectAllButtons();
   selectButton("blue");
   elementToChange.style.background = "lightblue";
 }
 
 function changeGreen() {
-  const elementToChange = document.getElementById("color-to-change");
+  const elementToChange = document.getElementById("color-to-change"),
+    buttonToUpdate = document.getElementById("green");
 
+  updateCount("green");
+  buttonToUpdate.textContent = "Green (" + buttonCount.green + ")";
   unSelectAllButtons();
   selectButton("green");
   elementToChange.style.background = "lightgreen";
@@ -36,7 +51,6 @@ function changeGreen() {
 
 function selectButton(buttonID) {
   document.getElementById(buttonID).classList.add("selected");
-  console.log("select button");
 }
 
 function unSelectAllButtons() {
@@ -45,15 +59,27 @@ function unSelectAllButtons() {
   document.getElementById("green").classList.remove("selected");
 }
 
+function updateCount(color) {
+  const newCount = buttonCount[color] + 1;
+  console.log(newCount);
+  buttonCount[color] = newCount;
+}
+
 function onLoad() {
   const randomNumber = Math.floor(Math.random() * 3);
 
   if (randomNumber === 0) {
     changePink();
+    buttonCount.pink = 0;
+    document.getElementById("pink").textContent = "Pink";
   } else if (randomNumber === 1) {
     changeBlue();
+    buttonCount.blue = 0;
+    document.getElementById("blue").textContent = "Blue";
   } else {
     changeGreen();
+    buttonCount.green = 0;
+    document.getElementById("green").textContent = "Green";
   }
 }
 
