70ed874127387541216cb9866acc7ef005e06a7d
Patrick Wilson <pmwilson333@gmail.com>
2020-06-24 16:06:30 -0700

    color buttons working

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 1- Create Hello Name Tag
 1- Link sript page to index.html
 1- Create event listener for name
-
 1- Create place for name to display
 1- Update input name in name place
+
 1- Create 3 Buttons to change color
 1- Set style to each color button click
 
@@ -16,8 +16,8 @@
 1- See it on the page
 1- see test console.log
 1- console.log name input
-
 1- console.log place for name to change
 1- see change on site
+
 1- console.log the 3 different buttons
 1- See color on page
--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 document.getElementById("name-input").addEventListener("keyup", updateName);
+document.getElementById("pink").addEventListener("click", changePink);
+document.getElementById("blue").addEventListener("click", changeBlue);
+document.getElementById("green").addEventListener("click", changeGreen);
 
 function updateName() {
   const newName = document.getElementById("name-input").value,
     placeToDisplay = document.getElementById("display-name");
 
   placeToDisplay.textContent = newName;
+}
+
+function changePink() {
+  const elementToChange = document.getElementById("color-to-change");
+
+  elementToChange.style.background = "pink";
+}
+
+function changeBlue() {
+  const elementToChange = document.getElementById("color-to-change");
+
+  elementToChange.style.background = "lightblue";
+}
+
+function changeGreen() {
+  const elementToChange = document.getElementById("color-to-change");
+
+  elementToChange.style.background = "lightgreen";
+}
+
+function onLoad() {
+  const randomNumber = Math.floor(Math.random()) * 3,
+    colors = [changePink(), changeBlue(), changeGreen()],
+    randomColor = colors[randomNumber];
 }
